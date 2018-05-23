3a4a1b39d01959184a317bbb10556c5974e78d11
kruoak <kruoak@ccollege.ac.th>
2018-05-23 15:31:06 +0700

    edit index

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,29 @@
 <template>
-  <div>Hello world chaiya phum</div>
+  <div>
+    <h1>Hello Vue</h1>
+    <div>
+      <nuxt-link to="/about">About</nuxt-link>
+    </div>
+    <div>
+      <button @click="gotoAbout">About</button>
+    </div>
+  </div>
 </template>
+
 <script>
 export default {
-  data() {
-    return {
-      id: 1,
-    }
+  methods: {
+    gotoAbout() {
+      this.$router.push('/about')
+      // this.$router.push({ path: '/about' })
+      // this.$router.push('/about?id=1')
+      // this.$router.push({ path: '/about', query: {id: 1} })
+      // this.$router.replace('/about')
+      // this.$router.replace({ path: '/about' })
+      // this.$router.replace('/about?id=1')
+      // this.$router.replace({ path: '/about', query: {id: 1} })
+      // this.$router.go(-1)
+    },
   },
 }
 </script>
